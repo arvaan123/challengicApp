1637f9ed3bc94f2d84bffa103ddfb48803ab1888
Jeni Bariya <jenny.arvaan@gmail.com>
2022-05-31 17:24:43 +0530

    change bottom navigation method

--- app/src/main/java/com/app/challengicapp/design/home/HomeActivity.kt
+++ app/src/main/java/com/app/challengicapp/design/home/HomeActivity.kt
@@ -7,6 +7,7 @@ import com.app.challengicapp.design.challengic.ChallengicFragment
 import com.app.challengicapp.design.laderboard.LaderboardFragment
 import com.app.challengicapp.design.search.SearchFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 
 class HomeActivity : AppCompatActivity() { 
@@ -17,30 +18,29 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        // loadFragment(Laderboard_frag())
         val laderBoardFrag = LaderboardFragment()
         val FragTrans = supportFragmentManager.beginTransaction()
         FragTrans.replace(R.id.container, laderBoardFrag, "")
         FragTrans.commit()
 
-        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav = findViewById(R.id.bottomNav)
 
-        bottomNav.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
+        bottomNav.setOnItemSelectedListener{
+            when (it.itemId) {
 
                 R.id.laderboard -> {
                     val laderboard = LaderboardFragment()
                     val FragTrans1 = supportFragmentManager.beginTransaction()
                     FragTrans1.replace(R.id.container, laderboard, "")
                     FragTrans1.commit()
-                    return@OnNavigationItemSelectedListener true
+                    return@setOnItemSelectedListener true
                 }
                 R.id.challengic -> {
                     val challengic = ChallengicFragment()
                     val FragTrans2 = supportFragmentManager.beginTransaction()
                     FragTrans2.replace(R.id.container, challengic, "")
                     FragTrans2.commit()
-                    return@OnNavigationItemSelectedListener true
+                    return@setOnItemSelectedListener true
 
                 }
                 R.id.search -> {
@@ -48,12 +48,12 @@ class HomeActivity : AppCompatActivity() {
                     val FragTrans3 = supportFragmentManager.beginTransaction()
                     FragTrans3.replace(R.id.container, search, "")
                     FragTrans3.commit()
-                    return@OnNavigationItemSelectedListener true
+                    return@setOnItemSelectedListener true
 
                 }
             }
             false
-        })
+        }
     }
 
     override fun onBackPressed() {
